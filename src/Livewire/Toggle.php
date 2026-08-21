b439632e1e2c2adc722ab544ecd39bb22fbc0a1c
Oleksandr Kokoriev <akokorev@lenderkit.com>
2026-08-21 21:35:19 +0300

    fix: toggle component

--- src/Livewire/Toggle.php
+++ src/Livewire/Toggle.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaravelUx\DarkMode\Livewire;
 
 use Illuminate\Contracts\View\View;
--- src/View/Directives/DarkModeScripts.php
+++ src/View/Directives/DarkModeScripts.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaravelUx\DarkMode\View\Directives;
 
 class DarkModeScripts
@@ -8,16 +10,26 @@ class DarkModeScripts
     {
         return <<<'HTML'
 <script>
-    document.documentElement.classList.toggle('dark', {{ Js::from(session('darkMode', false)) }})
-    document.addEventListener('alpine:init', () => {
-        Alpine.store('darkMode', {{ Js::from(session('darkMode', false)) }})
-        Alpine.watch(() => Alpine.store('darkMode'), value => {
-            document.documentElement.classList.toggle(
-                'dark',
-                typeof value === 'object' && value.initialValue !== undefined ? value.initialValue : value,
-            )
+    (() => {
+        let darkMode = {{ Js::from(session('darkMode', false)) }}
+
+        const applyDarkMode = (value = darkMode) => {
+            darkMode = typeof value === 'object' && value.initialValue !== undefined ? value.initialValue : value
+            document.documentElement.classList.toggle('dark', darkMode)
+        }
+
+        applyDarkMode()
+
+        document.addEventListener('dark-mode-toggled', event => applyDarkMode(event.detail.darkMode))
+        document.addEventListener('livewire:navigated', () => applyDarkMode())
+
+        document.addEventListener('alpine:init', () => {
+            Alpine.store('darkMode', darkMode)
+            Alpine.watch(() => Alpine.store('darkMode'), value => {
+                applyDarkMode(value)
+            })
         })
-    })
+    })()
 </script>
 HTML;
     }
